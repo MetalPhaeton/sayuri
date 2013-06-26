3e8360d9d0f7bfe7b8dd3dbf128b0b5942833781
hironori <phaeton2718@gmail.com>
2013-06-26 10:49:29 +0900

    CMakeLists.txtの修正、プロジェクトをビルド

---
+++ build/CMakeFiles/misaki.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hironori/Documents/Programming/Projects/misaki/src/chess_board.cpp" "/home/hironori/Documents/Programming/Projects/misaki/build/CMakeFiles/misaki.dir/src/chess_board.cpp.o"
+  "/home/hironori/Documents/Programming/Projects/misaki/src/chess_board_analyze.cpp" "/home/hironori/Documents/Programming/Projects/misaki/build/CMakeFiles/misaki.dir/src/chess_board_analyze.cpp.o"
+  "/home/hironori/Documents/Programming/Projects/misaki/src/chess_board_eval.cpp" "/home/hironori/Documents/Programming/Projects/misaki/build/CMakeFiles/misaki.dir/src/chess_board_eval.cpp.o"
+  "/home/hironori/Documents/Programming/Projects/misaki/src/chess_board_eval_weights.cpp" "/home/hironori/Documents/Programming/Projects/misaki/build/CMakeFiles/misaki.dir/src/chess_board_eval_weights.cpp.o"
+  "/home/hironori/Documents/Programming/Projects/misaki/src/chess_board_gen_move.cpp" "/home/hironori/Documents/Programming/Projects/misaki/build/CMakeFiles/misaki.dir/src/chess_board_gen_move.cpp.o"
+  "/home/hironori/Documents/Programming/Projects/misaki/src/chess_board_pondering.cpp" "/home/hironori/Documents/Programming/Projects/misaki/build/CMakeFiles/misaki.dir/src/chess_board_pondering.cpp.o"
+  "/home/hironori/Documents/Programming/Projects/misaki/src/chess_board_search.cpp" "/home/hironori/Documents/Programming/Projects/misaki/build/CMakeFiles/misaki.dir/src/chess_board_search.cpp.o"
+  "/home/hironori/Documents/Programming/Projects/misaki/src/chess_def.cpp" "/home/hironori/Documents/Programming/Projects/misaki/build/CMakeFiles/misaki.dir/src/chess_def.cpp.o"
+  "/home/hironori/Documents/Programming/Projects/misaki/src/chess_util.cpp" "/home/hironori/Documents/Programming/Projects/misaki/build/CMakeFiles/misaki.dir/src/chess_util.cpp.o"
+  "/home/hironori/Documents/Programming/Projects/misaki/src/game_record.cpp" "/home/hironori/Documents/Programming/Projects/misaki/build/CMakeFiles/misaki.dir/src/game_record.cpp.o"
+  "/home/hironori/Documents/Programming/Projects/misaki/src/misaki_debug.cpp" "/home/hironori/Documents/Programming/Projects/misaki/build/CMakeFiles/misaki.dir/src/misaki_debug.cpp.o"
+  "/home/hironori/Documents/Programming/Projects/misaki/src/move.cpp" "/home/hironori/Documents/Programming/Projects/misaki/build/CMakeFiles/misaki.dir/src/move.cpp.o"
+  "/home/hironori/Documents/Programming/Projects/misaki/src/opening_book.cpp" "/home/hironori/Documents/Programming/Projects/misaki/build/CMakeFiles/misaki.dir/src/opening_book.cpp.o"
+  "/home/hironori/Documents/Programming/Projects/misaki/src/pgn_parser.cpp" "/home/hironori/Documents/Programming/Projects/misaki/build/CMakeFiles/misaki.dir/src/pgn_parser.cpp.o"
+  "/home/hironori/Documents/Programming/Projects/misaki/src/test_main.cpp" "/home/hironori/Documents/Programming/Projects/misaki/build/CMakeFiles/misaki.dir/src/test_main.cpp.o"
+  "/home/hironori/Documents/Programming/Projects/misaki/src/transposition_table.cpp" "/home/hironori/Documents/Programming/Projects/misaki/build/CMakeFiles/misaki.dir/src/transposition_table.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ build/CMakeFiles/misaki.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+FILE(REMOVE_RECURSE
+  "libmisaki.a"
+)
