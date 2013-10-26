2e1486690434a970f8a6871aae674cafe9f6af4c
Ishibashi Hironori <phaeton2718@gmail.com>
2013-10-26 18:58:00 +0900

    起動オプションを作った
    --versionと--helpというオプションを作った。

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,41 @@
 #include <iostream>
-#include "init.h"
-#include "chess_engine.h"
+#include <cstring>
+#include <cstdlib>
+#include "chess_def.h"
+#include "init.h" #include "chess_engine.h"
 #include "uci_shell.h"
 
 int main(int argc, char* argv[]) {
-  // 初期化。
-  Sayuri::Init();
 
-  // エンジン準備。
-  std::unique_ptr<Sayuri::ChessEngine> engine_ptr(new Sayuri::ChessEngine());
-  std::unique_ptr<Sayuri::UCIShell>
-  shell_ptr(new Sayuri::UCIShell(engine_ptr.get()));
+  if ((argc >= 2)
+  && (std::strcmp(argv[1], "--help") == 0)) {
+    // ヘルプの表示。
+    std::cout << "使い方:" << std::endl;
+    std::cout << "\tsayuri [オプション]" << std::endl;
+    std::cout << "------------------------------" << std::endl;
+    std::cout << "オプション:" << std::endl;
+    std::cout << "\t--version" << std::endl;
+    std::cout << "\t\tバージョンを表示。" << std::endl;
+    std::cout << "\t--help" << std::endl;
+    std::cout << "\t\tヘルプを表示。" << std::endl;
+    
+  } else if ((argc >= 2)
+  && (std::strcmp(argv[1], "--version") == 0)) {
+    // バージョン番号の表示。
+    std::cout << Sayuri::ID_NAME << std::endl;
+  } else {
+    // プログラムの起動。
+    // 初期化。
+    Sayuri::Init();
 
-  // エンジン起動。
-  shell_ptr->Run();
+    // エンジン準備。
+    std::unique_ptr<Sayuri::ChessEngine> engine_ptr(new Sayuri::ChessEngine());
+    std::unique_ptr<Sayuri::UCIShell>
+    shell_ptr(new Sayuri::UCIShell(engine_ptr.get()));
 
-  return 0;
+    // エンジン起動。
+    shell_ptr->Run();
+  }
+
+  return EXIT_SUCCESS;
 }
