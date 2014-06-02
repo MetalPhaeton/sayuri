30b43d9f43f8393d50bda20768faf674757d03a1
Ishibashi Hironori <phaeton2718@gmail.com>
2014-06-02 19:56:34 +0900

    main.cppのDoxygen化

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,31 @@
-/*
-   main.cpp: メイン関数。
+/* The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Hironori Ishibashi
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
 
-   The MIT License (MIT)
-
-   Copyright (c) 2014 Hironori Ishibashi
-
-   Permission is hereby granted, free of charge, to any person obtaining a copy
-   of this software and associated documentation files (the "Software"), to
-   deal in the Software without restriction, including without limitation the
-   rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-   sell copies of the Software, and to permit persons to whom the Software is
-   furnished to do so, subject to the following conditions:
-
-   The above copyright notice and this permission notice shall be included in
-   all copies or substantial portions of the Software.
-
-   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-   IN THE SOFTWARE.
-*/
+/**
+ * @file main.cpp
+ * @author Hironori Ishibashi
+ * @brief メイン関数。
+ */
 
 #include <iostream>
 #include <cstring>
@@ -32,12 +35,18 @@
 
 #include "sayuri.h"
 
-// UCI出力を標準出力に出力。
+/**
+ * UCI出力を標準出力に出力するコールバック関数。
+ * @param message UCIShellからのメッセージ。
+ */
 void Print(const std::string& message) {
   std::cout << message << std::endl;
 }
 
-// コマンド入力ループ。
+/**
+ * 実行ループ。
+ * @param shell 実行するUCIShell。
+ */
 void Run(Sayuri::UCIShell& shell) {
   while (true) {
     std::string input;
@@ -51,6 +60,12 @@ void Run(Sayuri::UCIShell& shell) {
   }
 }
 
+/**
+ * メイン関数。
+ * @param argc コマンドの引数の数。
+ * @param argv コマンドの引数。
+ * @return 終了コード。
+ */
 int main(int argc, char* argv[]) {
   if ((argc >= 2)
   && (std::strcmp(argv[1], "--help") == 0)) {
