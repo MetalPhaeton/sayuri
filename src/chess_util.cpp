3e2652d5e45b7e5ce5b1a77ec6a45295e1336e8e
Ishibashi Hironori <phaeton2718@gmail.com>
2014-06-02 14:09:26 +0900

    chess_util.cppをDoxygen化

--- src/chess_util.cpp
+++ src/chess_util.cpp
@@ -1,28 +1,31 @@
-/*
-   chess_util.cpp: チェスの便利ツール。
+/* The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Hironori Ishibashi
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
 
-   The MIT License (MIT)
-
-   Copyright (c) 2014 Hironori Ishibashi
-
-   Permission is hereby granted, free of charge, to any person obtaining a copy
-   of this software and associated documentation files (the "Software"), to
-   deal in the Software without restriction, including without limitation the
-   rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-   sell copies of the Software, and to permit persons to whom the Software is
-   furnished to do so, subject to the following conditions:
-
-   The above copyright notice and this permission notice shall be included in
-   all copies or substantial portions of the Software.
-
-   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-   IN THE SOFTWARE.
-*/
+/**
+ * @file chess_util.cpp
+ * @author Hironori Ishibashi
+ * @brief Sayuri用便利ツールの実装。
+ */
 
 #include "chess_util.h"
 
@@ -32,9 +35,15 @@
 #include <random>
 #include "common.h"
 
+/** Sayuri 名前空間。 */
 namespace Sayuri {
+  /** 無名名前空間。 */
   namespace {
-    // ビットの個数を得る。
+    /** 
+     * 立っているビットの数を数える。
+     * @param bits 対象のビット。
+     * @return 立っているビットの数。
+     */
     int GetNumBits(unsigned int bits) {
       int num = 0;
       for (; bits; bits &= bits - 1) {
@@ -44,9 +53,9 @@ namespace Sayuri {
     }
   }
 
-  /****************/
-  /* static定数。 */
-  /****************/
+  // ========== //
+  // static定数 //
+  // ========== //
   constexpr Bitboard Util::SQUARE[NUM_SQUARES];
   constexpr Bitboard Util::FYLE[NUM_FYLES];
   constexpr Bitboard Util::RANK[NUM_RANKS];
@@ -64,9 +73,9 @@ namespace Sayuri {
   constexpr Bitboard Util::MAGIC_MASK_D[NUM_SQUARES];
   constexpr int Util::BLOCKER_MAP;
 
-  /************************/
-  /* Utilクラスの初期化。 */
-  /************************/
+  // ================== //
+  // Utilクラスの初期化 //
+  // ================== //
   void Util::InitUtil() {
     // num_bit16_table_[]を初期化する。
     InitNumBit16Table();
@@ -90,11 +99,10 @@ namespace Sayuri {
     InitRandom();
   }
 
-  /**********************************/
-  /* ビットを数えるときに使うもの。 */
-  /**********************************/
-  // 16ビットのビットの個数が入った配列。
-  // 引数には16ビットのパターンを入れる。
+  // ============================ //
+  // ビットを数えるときに使うもの //
+  // ============================ //
+  // 下位16ビットのビットの数が入った配列。 [下位16ビット]
   int Util::num_bit16_table_[0xffff + 1];
   // num_bit16_table_[]を初期化する。
   void Util::InitNumBit16Table() {
@@ -103,19 +111,16 @@ namespace Sayuri {
     }
   }
 
-  /**************/
-  /* マジック。 */
-  /**************/
-  // マジックのシフト。
-  // 0度と90度用。
-  // 45度と135度用。
-  // マジックのマスク。
-  // 0度と90度用。
-  // 45度と135度用。
-  // 各方向の攻撃の配列。
+  // ======== //
+  // マジック //
+  // ======== //
+  // 0度方向への攻撃の配列。 [マス][ブロッカーのパターン8ビット]
   Bitboard Util::attack_table_0_[NUM_SQUARES][BLOCKER_MAP + 1];  // 0度。
+  // 45度方向への攻撃の配列。 [マス][ブロッカーのパターン8ビット]
   Bitboard Util::attack_table_45_[NUM_SQUARES][BLOCKER_MAP + 1];  // 45度。
+  // 90度方向への攻撃の配列。 [マス][ブロッカーのパターン8ビット]
   Bitboard Util::attack_table_90_[NUM_SQUARES][BLOCKER_MAP + 1];  // 90度。
+  // 135度方向への攻撃の配列。 [マス][ブロッカーのパターン8ビット]
   Bitboard Util::attack_table_135_[NUM_SQUARES][BLOCKER_MAP + 1];  // 135度。
   // attack_table_***_[][]を初期化する。
   void Util::InitAttackTable() {
@@ -230,7 +235,8 @@ namespace Sayuri {
       }
     }
   }
-  // 45度座標のビットボードを通常の座標に戻す。
+
+  // 45度の座標を0度に逆変換する。
   Bitboard Util::Reverse45(Bitboard bitboard45) {
     // 通常座標に変換する。
     Bitboard bitboard = 0;
@@ -244,7 +250,7 @@ namespace Sayuri {
     // 返す。
     return bitboard;
   }
-  // 90度座標のビットボードを通常の座標に戻す。
+  // 90度の座標を0度に逆変換する。
   Bitboard Util::Reverse90(Bitboard bitboard90) {
     // 通常座標に変換する。
     Bitboard bitboard = 0;
@@ -258,7 +264,7 @@ namespace Sayuri {
     // 返す。
     return bitboard;
   }
-  // 135度座標のビットボードを通常の座標に戻す。
+  // 135度の座標を0度に逆変換する。
   Bitboard Util::Reverse135(Bitboard bitboard135) {
     // 通常座標に変換する。
     Bitboard bitboard = 0;
@@ -273,24 +279,24 @@ namespace Sayuri {
     return bitboard;
   }
 
-  /************************/
-  /* ビットボードの配列。 */
-  /************************/
-  // 直線の入った配列。
+  // ================== //
+  // ビットボードの配列 //
+  // ================== //
+  // 直線の入った配列。 [端点][端点]
   Bitboard Util::line_[NUM_SQUARES][NUM_SQUARES];
-  // ポーンの通常の動きの配列。
+  // ポーンの通常の動きの配列。 [サイド][マス]
   Bitboard Util::pawn_move_[NUM_SIDES][NUM_SQUARES];
-  // ポーンの2歩の動きの配列。
+  // ポーンの2歩の動きの配列。 [サイド][マス]
   Bitboard Util::pawn_2step_move_[NUM_SIDES][NUM_SQUARES];
-  // ポーンの攻撃筋の配列。
+  // ポーンの攻撃筋の配列。 [サイド][マス]
   Bitboard Util::pawn_attack_[NUM_SIDES][NUM_SQUARES];
-  // ナイトの動きの配列。
+  // ナイトの動きの配列。 [マス]
   Bitboard Util::knight_move_[NUM_SQUARES];
-  // ビショップの動きの配列。
+  // ビショップの動きの配列。 [マス]
   Bitboard Util::bishop_move_[NUM_SQUARES];
-  // ルークの動きの配列。
+  // ルークの動きの配列。 [マス]
   Bitboard Util::rook_move_[NUM_SQUARES];
-  // キングの動きの配列。
+  // キングの動きの配列。 [マス]
   Bitboard Util::king_move_[NUM_SQUARES];
   // line_[][]を初期化する。
   void Util::InitLine() {
@@ -535,10 +541,10 @@ namespace Sayuri {
     }
   }
 
-  /**********************/
-  /* その他の便利関数。 */
-  /**********************/
-  // 文字列を切り分ける。
+  // ================ //
+  // その他の便利関数 //
+  // ================ //
+  // 文字列をトークンに切り分ける。
   std::vector<std::string> Util::Split(std::string str, std::string delim,
   std::string delim_kept) {
     std::vector<std::string> ret;  // 結果。
@@ -595,7 +601,7 @@ namespace Sayuri {
     return std::move(ret);
   }
 
-  // ランダム関連の初期化。
+  // --- ランダム関連 --- //
   std::mt19937 Util::engine_;
   std::uniform_int_distribution<Hash> Util::dist_;
   void Util::InitRandom() {
