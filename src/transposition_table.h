5f2572b679136f0012b333a7fb11fad9c993b9ad
Ishibashi Hironori <phaeton2718@gmail.com>
2014-06-04 07:00:58 +0900

    transposition_table.hのDoxygen化

--- src/transposition_table.h
+++ src/transposition_table.h
@@ -1,28 +1,31 @@
-/*
-   transposition_table.h: トランスポジションテーブル。
+/* The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Hironori Ishibashi
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
 
-   The MIT License (MIT)
-
-   Copyright (c) 2014 Hironori Ishibashi
-
-   Permission is hereby granted, free of charge, to any person obtaining a copy
-   of this software and associated documentation files (the "Software"), to
-   deal in the Software without restriction, including without limitation the
-   rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-   sell copies of the Software, and to permit persons to whom the Software is
-   furnished to do so, subject to the following conditions:
-
-   The above copyright notice and this permission notice shall be included in
-   all copies or substantial portions of the Software.
-
-   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-   IN THE SOFTWARE.
-*/
+/**
+ * @file transposition_table.h
+ * @author Hironori Ishibashi
+ * @brief トランスポジションテーブル。
+ */
 
 #ifndef TRANSPOSITION_TABLE_H
 #define TRANSPOSITION_TABLE_H
@@ -33,172 +36,242 @@
 #include <cstddef>
 #include "common.h"
 
+/** Sayuri 名前空間。 */
 namespace Sayuri {
   class TranspositionTable;
 
-  // トランスポジションテーブルのエントリー。
+  /** トランスポジションテーブルのエントリー。 */
   class TTEntry {
     public:
-      /**************************/
-      /* コンストラクタと代入。 */
-      /**************************/
-      // [引数]
-      // pos_hash: ポジションのハッシュ。
-      // depth: 探索の深さ。
-      // to_move: 手番。
-      // value: 評価値。
-      // score_type: 評価値の種類。
-      // best_move: 最善手。
-      // mate_in: メイトまでの手数。
-      // table_age: トランスポジションテーブルの年齢。
-      TTEntry(Hash pos_hash, int depth, int value, ScoreType score_type,
+      // ==================== //
+      // コンストラクタと代入 //
+      // ==================== //
+      /**
+       * コンストラクタ。
+       * @param pos_hash ポジションのハッシュ。
+       * @param depth 残り深さ。
+       * @param score 評価値。
+       * @param score_type 評価値の種類。
+       * @param best_move 最善手。
+       * @param mate_in メイトまでの手数。 -1はメイトなし。
+       * @param table_age トランスポジションテーブルの年齢。
+       */
+      TTEntry(Hash pos_hash, int depth, int score, ScoreType score_type,
       Move best_move, int mate_in,  int table_age);
+      /** コンストラクタ。 */
       TTEntry();
+      /**
+       * コピーコンストラクタ。
+       * @param entry コピー元。
+       */
       TTEntry(const TTEntry& entry);
+      /**
+       * ムーブコンストラクタ。
+       * @param entry ムーブ元。
+       */
       TTEntry(TTEntry&& entry);
+      /**
+       * コピー代入演算子。
+       * @param entry コピー元。
+       */
       TTEntry& operator=(const TTEntry& entry);
+      /**
+       * ムーブ代入演算子。
+       * @param entry ムーブ元。
+       */
       TTEntry& operator=(TTEntry&& entry);
+      /** デストラクタ。 */
       virtual ~TTEntry() {}
 
-      /********************/
-      /* パブリック関数。 */
-      /********************/
-      // 自身が有効かどうか。
+      // ============== //
+      // パブリック関数 //
+      // ============== //
+      /**
+       * 内容が有効かどうか。
+       */
       explicit operator bool() const {return depth_ > -MAX_VALUE;}
 
-      /**************/
-      /* アクセサ。 */
-      /**************/
-      // ハッシュ。
+      // ======== //
+      // アクセサ //
+      // ======== //
+      /**
+       * アクセサ - ポジションのハッシュ。
+       * @return ポジションのハッシュ。
+       */
       Hash pos_hash() const {return pos_hash_;}
-      // 深さ。
+      /**
+       * アクセサ - 残り深さ。
+       * @return 残り深さ。
+       */
       int depth() const {return depth_;}
-      // 評価値。
+      /**
+       * アクセサ - 評価値。
+       * @return 評価値。
+       */
       int score() const {return score_;}
-      // 評価値の種類。
+      /**
+       * アクセサ - 評価値の種類。
+       * @return 評価値の種類。
+       */
       ScoreType score_type() const {return score_type_;}
-      // 最善手。
+      /**
+       * アクセサ - 最善手。
+       * @return 最善手。
+       */
       Move best_move() const {return best_move_;}
-      // メイトまでの手数。
+      /**
+       * アクセサ - メイトまでの手数。 -1はメイトなし。
+       * @return メイトまでの手数。
+       */
       int mate_in() const {return mate_in_;}
-      // トランスポジションテーブルの年齢。
+      /**
+       * アクセサ - トランスポジションテーブルの年齢。
+       * @return トランスポジションテーブルの年齢。
+       */
       int table_age() const {return table_age_;}
 
     private:
-      /****************/
-      /* メンバ変数。 */
-      /****************/
-      // ハッシュ。
+      // ========== //
+      // メンバ変数 //
+      // ========== //
+      /** ポジションのハッシュ。 */
       Hash pos_hash_;
-      // 探索のレベル。
+      /** 残り深さ。 */
       int depth_;
-      // 評価値。
+      /** 評価値。 */
       int score_;
-      // 評価値の種類。
+      /** 評価値の種類。 */
       ScoreType score_type_;
-      // 最善手。
+      /** 最善手。 */
       Move best_move_;
-      // メイトまでの手数。
+      /** メイトまでの手数。 */
       int mate_in_;
-      // 記録時のトランスポジションテーブルの年齢。
+      /** 記録時のトランスポジションテーブルの年齢。 */
       int table_age_;
   };
 
-  // トランスポジションテーブルのクラス。
+  /** トランスポジションテーブルのクラス。 */
   class TranspositionTable {
     public:
-      /**************************/
-      /* コンストラクタと代入。 */
-      /**************************/
-      // [引数]
-      // table_size: トランスポジションテーブルのサイズ指定。
+      // ==================== //
+      // コンストラクタと代入 //
+      // ==================== //
+      /**
+       * コンストラクタ。
+       * @param table_size テーブルのサイズ。 (バイト)
+       */
       TranspositionTable(std::size_t table_size);
+      /**
+       * コピーコンストラクタ。
+       * @param table コピー元。
+       */
       TranspositionTable(const TranspositionTable& table);
+      /**
+       * ムーブコンストラクタ。
+       * @param table ムーブ元。
+       */
       TranspositionTable(TranspositionTable&& table);
+      /**
+       * コピー代入演算子。
+       * @param table コピー元。
+       */
       TranspositionTable& operator=(const TranspositionTable& table);
+      /**
+       * ムーブ代入演算子。
+       * @param table ムーブ元。
+       */
       TranspositionTable& operator=(TranspositionTable&& table);
+      /** デストラクタ。 */
       virtual ~TranspositionTable() {}
+      /** コンストラクタ。 (削除) */
       TranspositionTable() = delete;
 
-      /********************/
-      /* パブリック関数。 */
-      /********************/
-      // テーブルに追加する。
-      // [引数]
-      // pos_hash: ハッシュ。
-      // depth: 探索の深さ。
-      // value: 評価値。
-      // score_type: 評価値の種類。
-      // best_move: 最善手。
-      // mate_in: メイトまでの手数。
-      void Add(Hash pos_hash, int depth, int value, ScoreType score_type,
+      // ============== //
+      // パブリック関数 //
+      // ============== //
+      /**
+       * テーブルにエントリーを追加。
+       * @param pos_hash ポジションのハッシュ。
+       * @param depth 残り深さ。
+       * @param score 評価値。
+       * @param score_type 評価値の種類。
+       * @param best_move 最善手。
+       * @param mate_in メイトまでの手数。 -1はメイトなし。
+       */
+      void Add(Hash pos_hash, int depth, int score, ScoreType score_type,
       Move best_move, int mate_in);
 
-      // 条件を満たすエントリーを得る。
-      // Lock()とUnlock()で挟んでロックする。
-      // [引数]
-      // pos_hash: ハッシュ。
-      // depth: 探索の深さ。
-      // [戻り値]
-      // 条件を満たすエントリー。
+      /**
+       * 条件を満たすエントリーを得る。
+       * @param pos_hash 条件のハッシュ。
+       * @param depth 条件の残り深さ。
+       * @return 条件を満たすエントリー。
+       */
       const TTEntry& GetEntry(Hash pos_hash, int depth) const;
 
-      // 年を取る。
+      /** 年を取る。 */
       void GrowOld() {age_++;}
 
-      // 大きさが何バイトか返す。
-      // [戻り値]
-      // サイズをバイト数で返す。
+      /**
+       * テーブルのサイズを返す。 (バイト)
+       * @return テーブルのサイズ。
+       */
       std::size_t GetSizeBytes() const {
         return num_entries_ * sizeof(TTEntry);
       }
 
-
-      // 使用されているエントリーのサイズを全体の何パーミルかを返す。
-      // [戻り値]
-      // エントリーのパーミル。
+      /**
+       * 使用済みエントリーの占める割合。 (パーミル)
+       * @return 使用済みエントリーの占める割合。
+       */
       int GetUsedPermill() const {
         return (num_used_entries_ * 1000) / num_entries_;
       }
 
-      // トランスポジションテーブルのロックを使う。
+      /** トランスポジションテーブルのロックでロックする。 */
       void Lock() {mutex_.lock();}
+      /** トランスポジションテーブルのロックのロックを解除する。。 */
       void Unlock() {mutex_.unlock();}
 
-      /**************/
-      /* アクセサ。 */
-      /**************/
-      // 年齢。
+      // ======== //
+      // アクセサ //
+      // ======== //
+      /**
+       * アクセサ - 年齢。
+       * @return 年齢。
+       */
       int age() const {return age_;}
 
     private:
-      // デバッグ用関数をフレンド。
+      /** フレンドのデバッグ用関数。 */
       friend int DebugMain(int argc, char* argv[]);
 
-      /**********************/
-      /* プライベート関数。 */
-      /**********************/
-      // テーブルのインデックスを得る。
-      // [引数]
-      // pos_hash: ポジションのハッシュ。
+      // ================ //
+      // プライベート関数 //
+      // ================ //
+      /**
+       * テーブルのインデックスを得る。
+       * @param ポジションのハッシュ。
+       * @return テーブルのインデックス。
+       */
       std::size_t GetTableIndex(Hash pos_hash) const {
         return pos_hash % num_entries_;
       }
 
-      /****************/
-      /* メンバ変数。 */
-      /****************/
-      // 無効なエントリー。
+      // ========== //
+      // メンバ変数 //
+      // ========== //
+      /** 無効なエントリー。 */
       const TTEntry null_entry_;
-      // エントリーの個数。
+      /** エントリーの個数。 */
       std::size_t num_entries_;
-      // 使用済みのエントリーの個数。
+      /** 使用済みのエントリーの個数。 */
       std::size_t num_used_entries_;
-      // エントリーを登録するテーブル。
+      /** エントリーを登録するテーブル。 */
       std::unique_ptr<TTEntry[]> entry_table_;
-      // 年齢。
+      /** 年齢。 */
       int age_;
-      // ミューテックス。
+      /** ミューテックス。 */
       std::mutex mutex_;
   };
 }  // namespace Sayuri
