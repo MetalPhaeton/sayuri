5ddc28f0d414eddfe479fa7fde4baaa1c3ead595
Ishibashi Hironori <phaeton2718@gmail.com>
2014-06-03 10:22:59 +0900

    params.cppのDoxygen化

--- src/params.cpp
+++ src/params.cpp
@@ -1,28 +1,31 @@
-/*
-   params.h: 探索アルゴリズム、評価関数を変更するパラメータのクラスの実装。
+/* The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Hironori Ishibashi
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
 
-   The MIT License (MIT)
-
-   Copyright (c) 2014 Hironori Ishibashi
-
-   Permission is hereby granted, free of charge, to any person obtaining a copy
-   of this software and associated documentation files (the "Software"), to
-   deal in the Software without restriction, including without limitation the
-   rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-   sell copies of the Software, and to permit persons to whom the Software is
-   furnished to do so, subject to the following conditions:
-
-   The above copyright notice and this permission notice shall be included in
-   all copies or substantial portions of the Software.
-
-   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-   IN THE SOFTWARE.
-*/
+/**
+ * @file params.cpp
+ * @author Hironori Ishibashi
+ * @brief 探索アルゴリズム、評価関数を変更するパラメータの実装。
+ */
 
 # include "params.h"
 
@@ -30,10 +33,11 @@
 #include <cstdint>
 #include "common.h"
 
+/** Sayuri 名前空間。 */
 namespace Sayuri {
-  /******************/
-  /* SearchParams。 */
-  /******************/
+  // ============ //
+  // SearchParams //
+  // ============ //
   // コンストラクタ。
   SearchParams::SearchParams() :
   ybwc_limit_depth_(4),
@@ -156,9 +160,9 @@ namespace Sayuri {
     }
   }
 
-  /****************/
-  /* EvalParams。 */
-  /****************/
+  // ========== //
+  // EvalParams //
+  // ========== //
   // コンストラクタ。
   EvalParams::EvalParams() :
   weight_mobility_(1.0, 3.0),
