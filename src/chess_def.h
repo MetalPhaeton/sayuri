9c9796f309e5065f349296ebbdfe467fbd86403c
Ishibashi Hironori <phaeton2718@gmail.com>
2014-12-14 11:06:21 +0900

    開発用にバージョン番号を変更

--- src/chess_def.h
+++ src/chess_def.h
@@ -41,7 +41,7 @@ namespace Sayuri {
   // エンジン情報 //
   // ============ //
   /** Sayuriのバージョン番号。 */
-  constexpr const char* ID_NAME = "Sayuri 2014.12.14";
+  constexpr const char* ID_NAME = "Sayuri 2014.12.14 devel";
   /** Sayuriの作者名。 */
   constexpr const char* ID_AUTHOR = "Hironori Ishibashi";
 
