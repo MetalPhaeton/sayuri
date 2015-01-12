2b7f09c5f69622ab4ba3956d1a2fa79dcf9ae895
Ishibashi Hironori <phaeton2718@gmail.com>
2015-01-12 18:13:09 +0900

    Android用にコンパイルできるようにした

--- src/chess_def.h
+++ src/chess_def.h
@@ -56,7 +56,8 @@ namespace Sayuri {
 
 #if defined(__arm__)  // ARMのCPU用定数。
   /** トランスポジションテーブルの最大サイズ。 (ARM用) */
-  constexpr std::size_t UCI_MAX_TABLE_SIZE = 1024ULL * 1024ULL * 1024ULL;
+  constexpr std::size_t UCI_MAX_TABLE_SIZE =
+  2ULL * 1024ULL * 1024ULL * 1024ULL;
 
 #elif defined(__i386__) || defined(_M_IX86)  // 32ビットCPU用定数。
   /** トランスポジションテーブルの最大サイズ。 (32ビットCPU) */
--- src/fen.cpp
+++ src/fen.cpp
@@ -35,6 +35,7 @@
 #include <algorithm>
 #include <cstddef>
 #include <set>
+#include <cstdlib>
 #include "common.h"
 
 /** Sayuri 名前空間。 */
@@ -290,7 +291,7 @@ namespace Sayuri {
   // 50手ルールの手数文字列を評価する。
   void FEN::EvalPly100(const std::string& ply_100_str) {
     try {
-      ply_100_ = std::stoi(ply_100_str);
+      ply_100_ = std::strtol(ply_100_str.c_str(), NULL, 0);
     } catch (...) {
       throw SayuriError("FENを解析できません。 in FEN::EvalPly100()");
     }
@@ -299,7 +300,7 @@ namespace Sayuri {
   // 手数文字列を評価する。
   void FEN::EvalPly(const std::string& ply_str) {
     try {
-      ply_ = std::stoi(ply_str) * 2;
+      ply_ = std::strtol(ply_str.c_str(), NULL, 0) * 2;
       if (to_move_ == WHITE) {
         ply_ -= 1;
       }
