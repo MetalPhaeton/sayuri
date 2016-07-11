8ab60a8b3840882659063a54e63ffe8a6fb8ffbb
Ishibashi Hironori <phaeton2718@gmail.com>
2016-07-11 20:24:44 +0900

    Sayulispアプリケーションbayesian-predictor.scmを追加

---
+++ Tools/BayesianPridictor/README.md
@@ -0,0 +1,110 @@
+Bayesian Predictor
+==================
+
+Predicts logit of candidate moves with Naive Bayes.  
+A move of the greatest logit is the best move predicted by Naive Bayes.
+
+Usage
+-----
+
+(1) Run with the following command.
+
+    $ sayuri --sayulisp bayesian-predictor.scm <PGN - training data>
+
+(2) Input FEN(EPD) of target positions into Standard Input.
+
+(3) This application prints each move and logit.
+
+### Example ###
+
+(1) Prepare PGN that is recorded 100 games of a grand master. (games100.pgn)
+
+(2) Prepare EPD of target positions. (positions.epd)
+
+    rnbq1rk1/pp2ppbp/2pp1np1/8/2PP4/2N2NP1/PP2PPBP/R1BQ1RK1 b - - sm Bf5;
+    rnbqkbnr/pp2pppp/2p5/3p4/3PP3/8/PPP2PPP/RNBQKBNR w KQkq d6 sm exd5;
+
+(3) Run the application and input EPD.
+
+    $ sayuri --sayulisp bayesian-predictor.scm games100.pgn < positions.epd
+
+(4) The application prints candidate moves of target positions and their logit.
+
+    rnbq1rk1/pp2ppbp/2pp1np1/8/2PP4/2N2NP1/PP2PPBP/R1BQ1RK1 b - - sm Bf5;
+    g8h8 -26.8877983291517
+    h7h6 -0.312330710579701
+    h7h5 -17.5822444275775
+    e7e6 -16.0394393503478
+    e7e5 -13.2909211632065
+    b7b6 -4.8060851496101
+    b7b5 -5.57216043987761
+    a7a6 -4.10237147774551
+    a7a5 0.687812231472485
+    g6g5 -42.2378554347392
+    d6d5 -6.15560178200187
+    c6c5 -17.228835102323
+    d8e8 -54.5385998733888
+    d8d7 -23.6476139655796
+    d8c7 -17.283123471267
+    d8b6 -32.1956885274665
+    d8a5 -11.9414970093864
+    f8e8 -6.70605048509202
+    c8d7 -16.4272456292174
+    c8e6 -26.7194017746249
+    c8f5 -24.5503467241813
+    c8g4 -1.79113841288506
+    c8h3 -609.498406362403
+    g7h8 -186.652255188352
+    g7h6 -60.2472739497237
+    b8d7 -71.2596246640096
+    b8a6 6.13750806231518
+    f6e8 -89.0667059228656
+    f6d7 -27.3061165426527
+    f6h5 -20.8642832286062
+    f6d5 -15.4484404049009
+    f6g4 -98.2932677211238
+    f6e4 -9.20394592967672
+    
+    rnbqkbnr/pp2pppp/2p5/3p4/3PP3/8/PPP2PPP/RNBQKBNR w KQkq d6 sm exd5;
+    e4d5 1.92597270022715
+    e1e2 -126.352436356938
+    e1d2 -264.270888984493
+    e4e5 -1.22018147337763
+    h2h4 -59.7914501192615
+    h2h3 -8.16934499895975
+    g2g4 -67.4066345217619
+    g2g3 4.84041474890144
+    f2f4 -6.05416028473775
+    f2f3 -7.89219363057848
+    c2c4 20.5121165224085
+    c2c3 -8.32198864142307
+    b2b4 -49.5350696375634
+    b2b3 -26.8358490339778
+    a2a4 -32.2186694806645
+    a2a3 -12.5055601976397
+    d1h5 -342.164402167898
+    d1g4 -137.469743249456
+    d1f3 -19.076575654753
+    d1d3 -63.2854844917056
+    d1e2 -21.9472168274493
+    d1d2 -33.6311532897358
+    f1a6 -399.021734905868
+    f1b5 -1.94619302133943
+    f1c4 -0.150027653821937
+    f1d3 5.85788361299489
+    f1e2 -24.117657696378
+    c1h6 -262.272831556404
+    c1g5 -18.0680565253959
+    c1f4 -37.2397712144844
+    c1e3 -7.92250499404898
+    c1d2 -45.3909793952942
+    g1h3 -617.537511849338
+    g1f3 33.490533999915
+    g1e2 -45.0466580525277
+    b1c3 25.4450643920725
+    b1a3 -330.78709679759
+    b1d2 1.60699696245538
+
+### Note! ###
+
+Sayulisp is very slow. Please wait for calculating patiently.
---
+++ Tools/BayesianPridictor/bayesian-predictor.scm
@@ -0,0 +1,188 @@
+;; The MIT License (MIT)
+;;
+;; Copyright (c) 2016 Hironori Ishibashi
+;;
+;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;; of this software and associated documentation files (the "Software"), to
+;; deal in the Software without restriction, including without limitation the
+;; rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+;; sell copies of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+;;
+;; The above copyright notice and this permission notice shall be included in
+;; all copies or substantial portions of the Software.
+;;
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+;; FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+;; IN THE SOFTWARE.
+
+;; Predicates next move with Naive Bayes.
+
+;; PGN object.
+(define pgn (gen-pgn ((input-stream (ref argv 1)) '@read)))
+
+;; Engine.
+(define engine (gen-engine))
+
+;; Note to move.
+(define (note->move engine note)
+        (define candidates (chess->number(engine '@get-candidate-moves)))
+        (define ret ())
+        (for (move candidates)
+             (if (equal? (engine '@move->note move) note) (set! ret move) ()))
+        ret)
+
+;; Current position and note to data.
+(define (pos->data engine note)
+        (define move (note->move engine note))
+        (if (null? move)
+            ()
+            (chess->number (list (engine '@get-to-move)
+                                 (engine '@get-all-pieces)
+                                 move))))
+
+;; Get FEN from PGN.
+(define (get-fen pgn)
+        (define headers (pgn '@get-current-game-headers))
+        (define ret ())
+        (for (elm headers)
+             (if (equal? (car elm) "FEN") (set! ret (car (cdr elm))) ()))
+        ret)
+
+;; Make data list of one game.
+(define (make-data-list engine pgn)
+        ;; Set start position
+        (define start-fen (get-fen pgn))
+        (if (null? start-fen)
+            (engine '@set-new-game)
+            (engine '@set-fen start-fen))
+
+        ;; Make data list.
+        (define white-list ())
+        (define black-list ())
+        (define ret ())
+        (define temp ())
+        (while (not (null? (pgn '@current-move)))
+               (set! temp (pos->data engine (pgn '@current-move)))
+               (if (null? temp)
+                   ()
+                   (begin (if (equal? (car temp) WHITE)
+                              (push-back! white-list temp)
+                              (push-back! black-list temp))
+                          (engine '@play-move (car (cdr (cdr temp))))
+                          )
+                   )
+               (pgn '@next-move))
+        (list white-list black-list))
+
+;; Make all data list.
+(define (make-all-data engine pgn)
+        (define white-list ())
+        (define black-list ())
+        (define result ())
+        (for (i (range (pgn '@length)))
+             (pgn '@set-current-game i)
+             (set! result (make-data-list engine pgn))
+             (set! white-list (append white-list (car result)))
+             (set! black-list (append black-list (car (cdr result))))
+             )
+        (list white-list black-list))
+
+;; FEN to side and position.
+(define (fen->pos engine)
+        (chess->number
+          (list (engine '@get-to-move) (engine '@get-all-pieces))))
+
+;; Make condition func list.
+(define (gen-conditions engine)
+        (define side-and-pos (fen->pos engine))
+        (define side (car side-and-pos))
+        (define pos (car (cdr side-and-pos)))
+        (define (gen-func side piece square)
+                (define lambda-expr
+                        `(lambda (data)
+                                 (define data-side (car data))
+                                 (define data-position (car (cdr data)))
+                                 (equal? (ref data-position ,square)
+                                         (quote ,piece))
+                                 ))
+                (eval lambda-expr))
+        (define ret ())
+        (for (square (range 64))
+             (push-back! ret (gen-func side (car pos) square))
+             (set! pos (cdr pos)))
+        ret)
+
+;; Make predicate.
+(define (gen-predicates candidates)
+        (define (gen-func move)
+                (define lambda-expr
+                        `(lambda (data)
+                                 (define data-move (car (cdr (cdr data))))
+                                 (equal? data-move (quote ,move))))
+                (eval lambda-expr))
+        (define ret ())
+        (for (move candidates)
+             (push-back! ret (gen-func move)))
+        ret)
+
+;; Do bayes.
+(define (do-bayes data-list pred conditions)
+        (apply bayes (cons data-list (cons pred conditions))))
+
+;; Move to PCN
+(define square-str
+        '("a1" "b1" "c1" "d1" "e1" "f1" "g1" "h1"
+          "a2" "b2" "c2" "d2" "e2" "f2" "g2" "h2"
+          "a3" "b3" "c3" "d3" "e3" "f3" "g3" "h3"
+          "a4" "b4" "c4" "d4" "e4" "f4" "g4" "h4"
+          "a5" "b5" "c5" "d5" "e5" "f5" "g5" "h5"
+          "a6" "b6" "c6" "d6" "e6" "f6" "g6" "h6"
+          "a7" "b7" "c7" "d7" "e7" "f7" "g7" "h7"
+          "a8" "b8" "c8" "d8" "e8" "f8" "g8" "h8"))
+(define piece-str '("" "p" "n" "b" "r" "q" "k"))
+(define (move->pcn move)
+        (string-append (ref square-str (car move))
+                       (ref square-str (car (cdr move)))
+                       (ref piece-str (car (cdr (cdr move))))))
+
+;; Main.
+(stderr "Loading...")
+(define data-list (make-all-data engine pgn))
+(stderr "done.\n")
+
+(define loop #t)
+(define input "")
+(define candidates ())
+(define conditions ())
+(define predicates ())
+(define data-index 0)
+(define logit 0)
+(while loop
+       (set! input (stdin '@read-line))
+       (if (null? input)
+           (set! loop #f)
+           (try ((engine '@set-fen input)
+                 (set! candidates
+                       (chess->number (engine '@get-candidate-moves)))
+                 (set! conditions (gen-conditions engine))
+                 (set! predicates (gen-predicates candidates))
+                 (set! data-index (-- (chess->number (engine '@get-to-move))))
+                 (display input)
+                 (for (pred predicates)
+                      (set! logit
+                            (do-bayes (ref data-list data-index)
+                                      pred
+                                      conditions))
+                      (display (move->pcn (car candidates))
+                               " "
+                               (to-string logit))
+                      (pop-front! candidates)
+                      )
+                 (stdout "\n"))
+                ())))
+
